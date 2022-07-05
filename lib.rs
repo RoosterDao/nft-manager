50f438f0e4fc70e36f2df7ac2bcea93ceaf48c21
theduck <ankan.anurag@gmail.com>
2022-07-05 21:15:25 +0200

    call create and mint nft

--- lib.rs
+++ lib.rs
@@ -3,6 +3,7 @@
 use ink_env::{Environment, AccountId};
 use ink_lang as ink;
 use scale::{Decode, Encode};
+use ink_prelude::vec::Vec;
 
 #[derive(scale::Encode, scale::Decode, Debug)]
 #[cfg_attr(feature = "std", derive(scale_info::TypeInfo))]
@@ -16,12 +17,20 @@ pub enum RCError {
     ErrorCode(RCErrorCode),
 }
 
+type CollectionId = u32;
+type NftId = u32;
 #[ink::chain_extension]
 pub trait RmrkExt {
     type ErrorCode = RCErrorCode;
 
-    #[ink(extension = 1201, returns_result = false)]
-    fn read_nft(caller_id: AccountId, collection_id: u32, nft_id: u32) -> bool;
+    #[ink(extension = 1, returns_result = false)]
+    fn read_nft(caller_id: AccountId, collection_id: CollectionId, nft_id: NftId) -> bool;
+
+    #[ink(extension = 2, returns_result = false)]
+    fn mint_nft(contract_address: AccountId, owner: AccountId, collection_id: CollectionId, metadata: Vec<u8>) -> NftId;
+
+    #[ink(extension = 3, returns_result = false)]
+    fn create_collection(contract_address: AccountId, metadata: Vec<u8>, symbol: Vec<u8>) -> NftId;
 }
 
 impl From<RCErrorCode> for RCError {
@@ -64,6 +73,7 @@ impl Environment for CustomEnvironment {
 
 #[ink::contract(env = crate::CustomEnvironment)]
 mod rmrk_extension {
+    use ink_prelude::vec::Vec;
     use super::{RCError};
 
     #[ink(storage)]
@@ -74,11 +84,21 @@ mod rmrk_extension {
         pub fn new() -> Self { Self {} }
 
 
-        /// Calls current_era() in the pallet-dapps-staking
         #[ink(message)]
         pub fn read_nft(&self, collection_id: u32, nft_id: u32) -> bool {
             let caller = self.env().caller();
             self.env().extension().read_nft(caller.clone(), collection_id, nft_id).map_err(|_| false).unwrap()
+        }
+
+        #[ink(message)]
+        pub fn mint_nft(&self, collection_id: u32, metadata: Vec<u8>) -> u32 {
+            let caller = self.env().caller();
+            self.env().extension().mint_nft(self.env().account_id(), caller.clone(), collection_id, metadata).map_err(|_| 200).unwrap()
+        }
+
+        #[ink(message)]
+        pub fn create_collection(&self, metadata: Vec<u8>, symbol: Vec<u8>) -> u32 {
+            self.env().extension().create_collection(self.env().account_id(), metadata, symbol).map_err(|_| 200).unwrap()
         }
     }
 }
