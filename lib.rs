95ee062e1a93546aff67a509519b8fc71780fe0d
theduck <ankan.anurag@gmail.com>
2022-07-07 21:24:33 +0200

    pass metadata and symbol

--- lib.rs
+++ lib.rs
@@ -1,12 +1,15 @@
 #![cfg_attr(not(feature = "std"), no_std)]
 
-use ink_env::{Environment, AccountId};
+use ink_env::{AccountId, Environment};
 use ink_lang as ink;
+use ink_prelude::vec::Vec;
 
 #[derive(scale::Encode, scale::Decode, Debug)]
 #[cfg_attr(feature = "std", derive(scale_info::TypeInfo))]
 pub enum RCErrorCode {
     Failed,
+    CollectionNotCreated,
+    CollectionAlreadyCreated
 }
 
 #[derive(scale::Encode, scale::Decode, Debug)]
@@ -25,10 +28,15 @@ pub trait RmrkExt {
     fn read_nft(caller_id: AccountId, collection_id: CollectionId, nft_id: NftId) -> bool;
 
     #[ink(extension = 2)]
-    fn mint_nft(contract_address: AccountId, owner: AccountId, collection_id: CollectionId) -> Result<NftId, RCError>;
+    fn mint_nft(
+        contract_address: AccountId,
+        owner: AccountId,
+        collection_id: CollectionId,
+        metadata: Vec<u8>
+    ) -> Result<NftId, RCError>;
 
     #[ink(extension = 3)]
-    fn create_collection(contract_address: AccountId) -> Result<CollectionId, RCError>;
+    fn create_collection(contract_address: AccountId, metadata: Vec<u8>, symbol: Vec<u8>) -> Result<CollectionId, RCError>;
 }
 
 impl From<RCErrorCode> for RCError {
@@ -71,32 +79,72 @@ impl Environment for CustomEnvironment {
 
 #[ink::contract(env = crate::CustomEnvironment)]
 mod rmrk_extension {
-    use super::{RCError};
+    use super::{RCError, RCErrorCode};
 
-    use ink_prelude::vec::Vec;
     #[ink(storage)]
-    pub struct RmrkExtension {}
+    pub struct RmrkExtension {
+        collection_id: Option<u32>,
+    }
 
     impl RmrkExtension {
         #[ink(constructor, payable)]
-        pub fn new() -> Self { Self {} }
-
+        pub fn new() -> Self {
+            Self {
+                collection_id: None,
+            }
+        }
 
         #[ink(message)]
         pub fn read_nft(&self, collection_id: u32, nft_id: u32) -> bool {
             let caller = self.env().caller();
-            self.env().extension().read_nft(caller, collection_id, nft_id).map_err(|_| false).unwrap()
+            self.env()
+                .extension()
+                .read_nft(caller, collection_id, nft_id)
+                .map_err(|_| false)
+                .unwrap()
         }
 
         #[ink(message)]
-        pub fn mint_nft(&mut self, collection_id: u32, _metadata: Vec<u8>) -> Result<u32, RCError> {
+        pub fn read_collection_id(&self) -> Option<u32> {
+            self.collection_id
+        }
+
+        #[ink(message)]
+        pub fn read_nft_id(&self) -> Option<u32> {
+            self.collection_id  
+        }
+
+        #[ink(message, payable)]
+        pub fn mint_nft(&mut self, metadata: ink_prelude::string::String) -> Result<u32, RCError> {
+            if self.collection_id == None {
+                return Err(RCError::ErrorCode(RCErrorCode::CollectionNotCreated))
+            }
             let caller = self.env().caller();
-            self.env().extension().mint_nft(self.env().account_id(), caller, collection_id)
+            self.env()
+                .extension()
+                .mint_nft(self.env().account_id(), caller, self.collection_id.unwrap(), metadata.into_bytes())
         }
 
-        #[ink(message)]
-        pub fn create_collection(&mut self, _metadata: Vec<u8>, _symbol: Vec<u8>) -> Result<u32, RCError> {
-            self.env().extension().create_collection(self.env().account_id())
+        #[ink(message, payable)]
+        pub fn create_collection(
+            &mut self,
+            metadata: ink_prelude::string::String,
+            symbol: ink_prelude::string::String,
+        ) -> Result<u32, RCError> {
+            if self.collection_id != None {
+                return Err(RCError::ErrorCode(RCErrorCode::CollectionAlreadyCreated))
+            }
+
+            let result: Result<u32, RCError> = self.env()
+                .extension()
+                .create_collection(self.env().account_id(), metadata.into_bytes(), symbol.into_bytes());
+
+            match result {
+                Ok(cid) => self.collection_id = Some(cid),
+                Err(_) => ()
+            }
+
+            return result
         }
     }
 }
